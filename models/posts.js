b201cfb71100ab98f524d25932a99a6a7889d3f7
MaoDuoc <mar.garridod@duocuc.cl>
2024-06-21 00:52:56 -0400

    Se define modelo y se crean las entradas del blog a la bd

---
+++ models/posts.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('./database');
+
+const Post = sequelize.define('Post', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        allowNull: false,
+        primaryKey: true
+    },
+    title: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    content: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    }
+});
+
+module.exports = Post;
